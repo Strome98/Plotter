3dde27106edbe5120d01ff74b849c7182eee8a54
Kreith Zsolt <70943534+Strome98@users.noreply.github.com>
2023-03-06 19:52:19 +0100

    Add files via upload

---
+++ PlotterFeladat/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Plotter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PlotterFeladat
+{
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlotterInditas_Click(object sender, RoutedEventArgs e)
+        {
+            Plotter.Plotter rajzgep=new Plotter.Plotter(800,600,6);
+
+            rajzgep.EszkozBetoltese(1, Colors.LightSeaGreen, 2);
+            rajzgep.EszkozBetoltese(2, Colors.Blue, 1);
+            rajzgep.EszkozBetoltese(6, Color.FromRgb(0,255,0),10);
+
+            rajzgep.Show();
+
+            rajzgep.FejMozgatas(100);
+            rajzgep.FejLe();
+            rajzgep.FejFel();
+            rajzgep.FejForditas(-55.7);
+            rajzgep.FejMozgatas(new Point(300,200));
+            rajzgep.EszkozCsere(5);
+            rajzgep.Close();
+
+        }
+            
+    }
+    
+}
